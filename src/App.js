e92da19cb731ed6c578f3e168c9e8d9cabdc2e18
Luke <luke.sun@map8.zone>
2023-04-19 08:32:53 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Byeeee There!
+                    I was changed on the Feature branch
                 </a>
             </header>
         </div>
